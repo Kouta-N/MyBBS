b200d5295167968e6bb5d6e0b9118fc14a0afe48
kakashi <nk29time@gmail.com>
2021-06-14 15:32:17 +0900

    finish

--- MyBBS/resources/views/posts/show.blade.php
+++ MyBBS/resources/views/posts/show.blade.php
@@ -10,13 +10,59 @@
     <h1>
         <span>{{ $post->title }}</span>
         <a href="{{ route('posts.edit',$post) }}">[Edit]</a>
-        <form method="post" action="{{ route('posts.destroy',$post) }}">
+        <form method="post" action="{{ route('posts.destroy',$post) }}" id="delete_post">
             @method('DELETE')
             @csrf
             <button class="btn">[x]</button>
         </form>
     </h1>
     <p>{!! nl2br(e($post->body)) !!}</p>
+
+<h2>Comments</h2>
+<ul>
+    <li>
+        <form method="post" action="{{ route('comments.store',$post) }}" class="comment-form">
+            @csrf
+            <input type="text" name="body">
+            <button>Add</button>
+        </form>
+    </li>
+    @foreach($post->comments()->latest()->get() as $comment)
+    <li>
+        {{ $comment->body }}
+        <form method="post" action="{{ route('comments.destroy',$comment) }}" class="delete-comment">
+            @method('DELETE')
+            @csrf
+
+            <button class="btn">[x]</button>
+    </li>
+    @endforeach
+</ul>
+
+    <script>
+        'use strict';
+        {
+            document.getElementById('delete_post').addEventListener('submit',e => {
+                e.preventDefault();
+                if(!confirm('Sure to delete?')){
+                    return;
+                }
+                e.target.submit();
+            })
+
+            document.querySelectorAll('.delete-comment').forEach(form => {
+                form.addEventListener('submit',e => {
+                    e.preventDefault();
+
+                    if(!confirm('Sure to delete?')){
+                        return;
+                    }
+
+                    form.submit();
+                });
+            });
+        }
+    </script>
 </x-layout>
 
 
